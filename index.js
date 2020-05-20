0949eb3433e273eef364ec0fd74c23cba0bfee30
Dmitry Kuchukov <kudim84@yandex.ru>
2020-05-20 22:27:34 +0300

    Добавляет простой сервер

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const http = require(`http`);
+const fs = require(`fs`);
+const path = require(`path`);
 
 const server = http.createServer((req, res) => {
    if (req.method === `GET`) {
        res.writeHead(200, {
-          'Content-Type': `text/html`
+          'Content-Type': `text/html; charset=utf-8`
        });
-       res.end(`
-       <h1>Form</h1>
-       <form method="post" action="/">
-       <input type="text" name="title" />
-       <button type="submit">Send</button>
-       </form>
-       `)
+
+       if (req.url === `/`) {
+           fs.readFile(
+               path.join(__dirname, `views`, `index.html`),
+               `utf-8`,
+               (err, content) => {
+                   if(err) {
+                       throw err;
+                   }
+                   res.end(content);
+               }
+           )
+       } else if(req.url === `/about`){
+           fs.readFile(
+               path.join(__dirname, `views`, `about.html`),
+               `utf-8`,
+               (err, content) => {
+                   if(err) {
+                       throw err;
+                   }
+                   res.end(content);
+               }
+           )
+       }
+       else if (req.url === `/api/users`) {
+           res.writeHead(200, {
+               'Content-Type': `text/json`
+           });
+
+           const users = [
+               {name: `Dima`, age: 36},
+               {name: `Jane`, age: 32}
+           ];
+
+           res.end(JSON.stringify(users));
+       }
    } else if (req.method === 'POST') {
        const body = [];
        res.writeHead(200, {
@@ -25,8 +56,7 @@ const server = http.createServer((req, res) => {
 
        req.on(`end`, () => {
           const message = body.toString().split(`=`)[1];
-           res.end(`<h1>Ваше сообщение: ${message}</h1>
-       `)
+           res.end(`<h1>Ваше сообщение: ${message}</h1>`)
        });
 
    }
